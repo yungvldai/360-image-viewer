f573bb4a86f049b9ed1242379d0bf1bc3e072c33
Vladislav.Ivanov <vladislav.ivanov@onetwotrip.com>
2020-11-24 22:37:20 +0300

    initial

---
+++ src/index.js
@@ -0,0 +1,24 @@
+
+import create360Viewer from '360-image-viewer';
+import canvasFit from 'canvas-fit';
+
+import './styles.scss';
+
+const root = document.querySelector('#root');
+const image = new Image();
+
+const params = new URLSearchParams(window.location.search);
+const path = params.get('path');
+
+image.src = path;
+
+image.onload = () => {
+  const viewer = create360Viewer({
+    image
+  });
+  root.appendChild(viewer.canvas);
+  const fit = canvasFit(viewer.canvas, window, window.devicePixelRatio);
+  window.addEventListener('resize', fit, false);
+  fit();
+  viewer.start();
+};
